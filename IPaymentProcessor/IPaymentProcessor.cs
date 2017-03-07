9ce0ebd88e29e621f7f41eecf76f1b3d4081e29d
leewsimpson <leewsimpson@gmail.com>
2017-03-07 22:28:55 +1000

    Add project files.

---
+++ IPaymentProcessor/IPaymentProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Remoting;
+
+namespace BankingAssociation.Interfaces
+{
+    using System.ServiceModel;
+    using System.Threading.Tasks;
+
+    // Define a service contract.
+    [ServiceContract]
+    public interface IPaymentProcessor : IService
+    {
+        Task<long> Get();
+    }
+}
---
+++ WebApi/Controllers/CounterController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using BankingAssociation.Interfaces;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using System;
+using Microsoft.ServiceFabric.Services.Client;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [ServiceRequestActionFilter]
+    public class CounterController : ApiController
+    {
+        // GET api/counter
+        public async Task<long> Get()
+        {
+            IPaymentProcessor counter = ServiceProxy.Create<IPaymentProcessor>(new Uri("fabric:/BankingAssociation.Payment/PaymentProcessor"), new ServicePartitionKey(0));
+
+            long count = await counter.Get();
+
+            return count;
+        }
+    }
+}
